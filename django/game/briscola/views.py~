4307d7dfae6a73a7c2d698892b51c03c6b7e9e5c
FilicMia <filicmia@gmail.com>
2016-12-18 23:18:06 +0100

    frontPage displayed with django

---
+++ django/game/briscola/views.py~
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request,'friws/index.html', {'victim':'happy people'})
---
+++ django3.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mia/Dokumenti/WebProgramming/MiaFilic_WebProgramming/django3.5/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
