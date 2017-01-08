15c27f010f1e5d54c36d717fdef453fcc9b39e75
FilicMia <filicmia@gmail.com>
2017-01-08 21:17:52 +0100

    commit

---
+++ django/myapp/login/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from login.views import *
+
+urlpatterns = [
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', views.logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', views.register),
+    url(r'^register/success/$', views.register_success),
+    url(r'^home/$', views.home),
+]
